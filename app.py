15b5871db4254b984249c6c112eb2280ef4ca0e4
MysteriusEnergy <jhonmariorestrepo890@gmail.com>
2025-07-07 15:16:09 -0500

    update first version

---
+++ app.py
@@ -0,0 +1,68 @@
+import tkinter as tk
+import os
+import sys
+
+from tkinter import messagebox
+
+# Función para centrar la ventana en la pantalla 
+def centrar_ventana_en_pantalla(ventana, ancho, alto):
+    ventana.update_idletasks()
+    screen_width = ventana.winfo_screenwidth()
+    screen_height = ventana.winfo_screenheight()
+    x = (screen_width // 2) - (ancho // 2)
+    y = (screen_height // 2) - (alto // 2)
+    ventana.geometry(f"{ancho}x{alto}+{x}+{y}")
+    
+# Función para obtener la ruta absoluta del recurso 
+def resource_path(relative_path):
+    """Obtener la ruta absoluta del recurrso"""
+    try:
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+    return os.path.join(base_path, relative_path)
+        
+# Función para convertir la ruta 
+def convertir_ruta():
+    ruta = entrada.get()
+    if ruta:
+        ruta_convertida = ruta.replace("\\", "/")
+        salida.delete(0, tk.END)
+        salida.insert(0, ruta_convertida)
+    else:
+        messagebox.showinfo("Información", "Por favor, introduce una ruta para convertir")
+        
+def copiar_ruta():
+    ruta_convertida = salida.get()
+    if ruta_convertida:
+        ventana.clipboard_clear()
+        ventana.clipboard_append(ruta_convertida)
+                
+    else:
+        messagebox.showinfo("Información", "No hay ruta convertida para copiar")
+
+# Ventana principal
+ventana = tk.Tk()
+ventana.title("Conversor de rutas")
+ventana.config(bg="#939d9d")
+ventana.iconbitmap(resource_path("assets/icon.ico"))
+
+centrar_ventana_en_pantalla(ventana, 500, 205)
+
+# Etiqueta y campo de entrada
+tk.Label(ventana, text="Pega la ruta: ", bg="#939d9d", fg="white", font=("Arial", 10, "bold")).pack(pady=5)
+entrada = tk.Entry(ventana, width=60)
+entrada.pack(pady=5)
+
+# Botón para convertir
+tk.Button(ventana, text="Convertir", command=convertir_ruta, font=("Arial", 10)).pack(pady=5)
+
+# Campo de salida
+tk.Label(ventana, text="Ruta convertida: ", bg="#939d9d", fg="white", font=("Arial", 10, "bold")).pack(pady=5)
+salida = tk.Entry(ventana, width=60)
+salida.pack(pady=5)
+
+# Botón para copiar
+tk.Button(ventana, text="Copiar", command=copiar_ruta, font=("Arial", 11)).pack(pady=5)
+
+ventana.mainloop()
